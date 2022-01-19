fd71512f35b60a7a773b7e861888f0ce21c03f55
Dragica Ljubisavljevic <gagaljubisavljevic111@gmail.com>
2022-01-19 22:28:50 +0100

    Kreirana indeks stranica

--- indeks.php
+++ indeks.php
@@ -1,6 +1,84 @@
 <?php
 
-echo("aaa");
+require "dbBroker.php";
+require "model/user.php";
 
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+   
+    $korisnik = new User(1, $uname, $upass);
+    
+    $odg = User::logInUser($korisnik, $conn); 
+
+    if($odg->num_rows==1){
+        echo  `
+        <script>
+        console.log( "Uspešno ste se prijavili");
+        </script>
+        `;
+        $_SESSION['user_id'] = $korisnik->id;
+        header('Location: home.php');
+        exit();
+    }else{
+        echo `
+        <script>
+        console.log( "Niste se prijavili!");
+        </script>
+        `;
+    }
+
+}
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  
+<div class="container">
+	<div class="screen">
+		<div class="screen__content">
+			<form class="login" method="POST" action="#">
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<input type="text" name="username" class="login__input" placeholder="Username">
+				</div>
+				<div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<input type="password" name="password" class="login__input" placeholder="Password">
+				</div>
+				<button type="submit" class="button login__submit" name="submit">
+					<span class="button__text">LOG IN</span>
+					<i class="button__icon fas fa-chevron-right"></i>
+				</button>				
+			</form>
+			<div class="social-login">
+				<h3>Aplikacija za zakazivinje termina u kozmetickom salonu</h3>
+				<div class="social-icons">
+					<a href="#" class="social-login__icon fab fa-instagram"></a>
+					<a href="#" class="social-login__icon fab fa-facebook"></a>
+					<a href="#" class="social-login__icon fab fa-twitter"></a>
+				</div>
+			</div>
+		</div>
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
+</div>
+
+</body>
+</html>
+
